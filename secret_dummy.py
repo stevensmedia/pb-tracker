f29d037ebeb989e5f434c6df79a5520b708ca7a0
Neil Stevens <neil@stevensmedia.us>
2015-04-09 19:19:44 -0400

    Get secret.py ready for deploy

--- secret_dummy.py
+++
@@ -1,7 +0,0 @@
-# secret.py
-# Author: Richard Gibson
-#
-# Rename this file to secret.py and change the secret before deployment. This
-# is used for secure login (see util.check_secure_val()).
-
-SECRET = 'foo'
